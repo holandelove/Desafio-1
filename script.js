74ed1d7a3fbce04d6cb36dbe2aabfc2ecb233a97
holandelove <86680275+holandelove@users.noreply.github.com>
2021-07-18 17:15:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const form = document.getElementById('form')
 
 form.addEventListener('submit', (e) => {
 
-// e.preventDefault();     
-
-
-
 let nome = document.getElementById('nome').value;
 let celular = document.getElementById('celular').value;
 let email = document.getElementById('email').value;
